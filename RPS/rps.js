763b47c7bfdc389831806fc9552845e948389609
Boy G <aghetsehgabrielchefor1@gmail.com>
2025-02-27 16:34:04 +0100

    background image for the game available

--- RPS/rps.js
+++ RPS/rps.js
@@ -2,10 +2,14 @@ const playerText = document.querySelector("#playerText");
 const computerText = document.querySelector("#computerText");
 const resultText = document.querySelector("#resultText");
 const choiceBtns = document.querySelectorAll(".choiceBtn");
+const playerScore = document.querySelector("#playerScore");
+const computerScore = document.querySelector("#computerScore"); 
 
 // lets create 3 variable
-let player; // for player choice 
-let computer; // for computer choice
+let pScore = 0; // where pScore stands for player's score which is Boy G
+let cScore = 0;  // same logic here for computer
+let player; 
+let computer; 
 let result;
 
 choiceBtns.forEach(button => button.addEventListener('click', () => {
@@ -15,6 +19,9 @@ choiceBtns.forEach(button => button.addEventListener('click', () => {
     playerText.textContent = `Boy G: ${player}`;
     computerText.textContent = `Computer: ${computer}`;
     resultText.textContent = checkWinner();
+
+    playerScore.textContent = `score: ${pScore}`;
+    computerScore.textContent = `score: ${cScore}`;
 }));
 
 function computerTurn(){
@@ -38,13 +45,19 @@ function checkWinner() {
     if(player == computer){
         return "Draw"
     }
-    else if(computer == "ROCK") {
-        return ( player == "PAPER") ? "You win!🏆" : "You Lose😔"
+     if(computer == "ROCK" && player == "PAPER") {
+        pScore++;
+        return ( "You win!🏆" );
     }
-    else if(computer == "PAPER") {
-        return ( player == "SCISSORS") ? "You win!🏆" : "You Lose😔"
+     if(computer == "PAPER" && player == "SCISSORS") {
+        pScore++;
+        return ( "You win!🏆" );
     }
-    else if(computer == "SCISSORS") {
-        return ( player == "ROCK") ? "You win!🏆" : "You Lose😔"
+     if(computer == "SCISSORS" && player == "ROCK") {
+        pScore++;
+        return ( "You win!🏆" ) 
+    } else {
+        cScore++
+        return ( "You Lose😔");
     }
  } 
